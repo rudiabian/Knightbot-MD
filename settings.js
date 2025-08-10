240b12f51875dcd3bb4982a8e9b22ac8e5000dde
rudiabian <rudi.rahmad7913@gmail.com>
2025-08-11 07:40:25 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  botOwner: 'bang Abian', // Your name
+  ownerNumber: '60142808841', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: '367f54b551248d41db6889c9',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
